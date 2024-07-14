6843a0d4bd22c54ad8238660cf42699e811af396
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-07-15 00:17:57 +0530

    feat: Add Button and Card components

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const Button = () => {
+interface ButtonProps {
+  type: 'link' | 'button' | 'submit';
+  text: string;
+  link?: string;
+  action?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ type, text, link, action }) => {
+  if (type === 'link' && link) {
+    return (
+      <Link to={link} className="px-4 py-2 bg-black text-white rounded">
+        {text}
+      </Link>
+    );
+  }
+
   return (
     <button
-      className="w-20 h-20 bg-black text-white"
-      onClick={() => alert(`Hello from your  app!`)}
+      type={type === 'submit' ? 'submit' : 'button'}
+      className="px-4 py-2 bg-black text-white rounded"
+      onClick={action}
     >
-        Click me
+      {text}
     </button>
   );
 };
+
+
--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from "react";
 
-export const  Card = ()  => {
-  return (
-    <div>card</div>
-  )
+interface CardProps {
+  title: string;
+  description: string;
+  coverImage: string;
+  children: React.ReactNode;
 }
 
+export const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  coverImage,
+  children,
+}) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4 w-64 h-96">
+      <img src={coverImage} alt={title} className="w-full h-32 object-cover" />
+      <div className="p-4">
+        <h2 className="text-xl font-bold text-center mb-2">{title}</h2>
+        <p className="text-center text-gray-600 mb-4">{description}</p>
+        {children}
+      </div>
+    </div>
+  );
+};
